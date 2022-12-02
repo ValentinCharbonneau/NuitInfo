1480e36485671e1f9664d07a4d8f16534ffeedbe
Clement <clement_moreau@outlook.fr>
2022-12-02 03:10:13 +0100

    testimony approval

--- src/Controller/TestimonyController.php
+++ src/Controller/TestimonyController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Testimony;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestimonyController extends AbstractController
 {
-    #[Route('/testimony', name: 'app_testimony')]
+    #[Route('/temoigner', name: 'app_testimony')]
     public function index(): Response
     {
-        return $this->render('testimony/index.html.twig', [
-            'controller_name' => 'TestimonyController',
+        return $this->render('testimony/index.html.twig');
+    }
+
+    #[Route('/temoignages', name: 'app_approval')]
+    public function approval(): Response
+    {
+        $test = [];
+        return $this->render('testimony/approval.html.twig', [
+            'temoignages' => $test,
         ]);
     }
 }
